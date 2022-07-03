2d4d0342b87e66dd5d70f500f0010a292c40fc9e
Kouff <thematuhaimmortal@gmail.com>
2022-07-04 01:57:29 +0300

    add the model Link

--- links/models.py
+++ links/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Link(models.Model):
+    link = models.URLField(max_length=500)
+    slug = models.SlugField(max_length=7, unique=True)
+    redirect_count = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
